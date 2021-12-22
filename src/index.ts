4b53cf579d8269005c0423af3fab43cce8ee1715
kobori <46224993+TomoroKobori@users.noreply.github.com>
2021-12-23 08:54:49 +0900

    feat: projectsの定義を追加

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,43 @@ import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
 
 const typeDefs = gql`
+  type Project {
+    id: Int!
+    title: String
+  }
   type Query {
-		hello: String
+		project(id: Int): Project
+    projects(title: String): [Project]
 	}
 `;
 
+const projects = [
+  {
+    id: 1,
+    title: 'title1'
+  },
+  {
+    id: 2,
+    title: 'title2'
+  },
+  {
+    id: 3,
+    title: 'title3'
+  },
+  {
+    id: 4,
+    title: 'title4'
+  },
+]
+
 const resolvers = {
 	Query: {
-		hello: () => 'Hello GraphQL!',
+    project: (_: any, { id }: { id: number }) => {
+      return projects.find(project => project.id == id)
+    },
+    projects: (_: any, { title }: { title: string }) => {
+      return projects.filter(project => project.title.includes(title))
+    }
 	},
 };
 
